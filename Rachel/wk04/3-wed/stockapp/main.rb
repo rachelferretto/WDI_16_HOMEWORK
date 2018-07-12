58a0911a58ca879456c91b44fc853d71f8b6dd1a
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-07-12 18:00:22 +1000

    99_bottles

--- Rachel/wk04/3-wed/stockapp/main.rb
+++ Rachel/wk04/3-wed/stockapp/main.rb
@@ -2,6 +2,7 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'stock_quote'
+#http://localhost:4567/home
 
 get '/home' do
     "<h1>Stock Performance</h1>"
@@ -9,7 +10,7 @@ get '/home' do
 end
 
 get '/stocksearch' do
-    @price = StockQuote::Stock.quote(params[:stockcode]).close
+    @price = StockQuote::Stock.quote(params[:stockcode]) #params filters the stockcode in a hash
     erb(:stocksearch)
 end
 
