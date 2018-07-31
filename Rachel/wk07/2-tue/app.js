d3a2858ec0110695d7a0e12ea5fa8e5acdb92449
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-07-31 18:11:18 +1000

    update css

--- Rachel/wk07/2-tue/app.js
+++ Rachel/wk07/2-tue/app.js
@@ -1,26 +1,16 @@
 console.log('search movie using ajax');
 
-
-
 var searchMovies = document.querySelector('.search');
 
 
-
-
 function onSuccess(res) {
-    
     res = res.Search
     var movie = document.querySelector('.search-result')
     for (i = 0; i <= res.length; i++) {
         var result = movie.appendChild(document.createElement("p"))
         result.textContent = res[i].Title
     }
-    
-
 };
-
-
-
 
 
 searchMovies.addEventListener('click', function() {
