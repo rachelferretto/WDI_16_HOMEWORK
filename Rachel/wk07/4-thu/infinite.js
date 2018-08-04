d28460422fa268abec34168951e93ee33f9b630c
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-08-04 21:37:13 +1000

    update css, WIP

--- Rachel/wk07/4-thu/infinite.js
+++ Rachel/wk07/4-thu/infinite.js
@@ -1,40 +1,62 @@
 $(document).ready(function() {
     console.log('search for a giphy');
-    var offset = 0;
+    var offset = 1;
 
     const onSuccess = (res) => {
         var gif = document.querySelector('.search-result')
         for (i = 0; i <= 10; i++) {
-        var giphyUrl = res.data[i].images.fixed_height.url
-        result = gif.appendChild(document.createElement("img"))
-        result.src = giphyUrl
+            var displayGif = gif.appendChild(document.createElement("img"))
+            displayGif.src = res.data[i].images.fixed_height.url
+            displayGif.classList.add("giphy")  
         }
+
+        var modal = document.getElementById('myModal');
+        function getModal() {
+            modal.style.display = "block";
+            var modalCaption = modal.appendChild(document.createElement("p"))
+            modalCaption.textContent = event.target.src
+            modalCaption.id = "caption"
+
+            var modalImg = modal.appendChild(document.createElement("img"))
+            modalImg.src = event.target.src
+            modalImg.classList.add("modal-image") 
+
+            var span = document.getElementsByClassName("close")[0];
+            span.onclick = function() { 
+            $("#caption").remove()
+            $(".modal-image").remove()
+            modal.style.display = "none";
+            }
+        }
+
+        $(".giphy").on('click', function(event) {
+            getModal(event)
+        })
     }
 
     function getResults() {
         var query = document.getElementById("search-text").value
         $.ajax({
             method: 'get',
-            url: "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=k9mr6XS5bWv8chZJT1EhPGoLeYCLRuvX&offset=" + offset, 
+            url: "http://api.giphy.com/v1/gifs/search?q=" + query + "&api_key=k9mr6XS5bWv8chZJT1EhPGoLeYCLRuvX&offset=" + offset,
             dataType: 'json'
         }).done(onSuccess) 
     }
 
-
     $(".search").on('click', function() {
         event.preventDefault()
         getResults()
+        searchResultInstructions = document.querySelector("p")
+        searchResultInstructions.classList.remove('hidden')
     })
-
 
     $(window).scroll(function() {
         if($(window).scrollTop() == $(document).height() - $(window).height()) {
-            offset = offset + 11
+            offset = offset + 10
             getResults()
         }
     })
 })
-
     
 
 
