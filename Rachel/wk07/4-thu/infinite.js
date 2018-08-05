a65dc7b34b031c11a36a36ed11de2dbe2614189a
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-08-05 14:32:54 +1000

    update css and add reset button

--- Rachel/wk07/4-thu/infinite.js
+++ Rachel/wk07/4-thu/infinite.js
@@ -2,31 +2,33 @@ $(document).ready(function() {
     console.log('search for a giphy');
     var offset = 1;
 
+    var modal = document.getElementById('myModal');
+    var hideResetBtn = document.querySelector("#reset")
+
+    function getModal(event) {
+        $("#caption").remove()
+        $(".modal-image").remove()
+        modal.style.display = "block";
+        var modalCaption = modal.appendChild(document.createElement("p"))
+        modalCaption.textContent = event.target.src
+        modalCaption.id = "caption"
+
+        var modalImg = modal.appendChild(document.createElement("img"))
+        modalImg.src = event.target.src
+        modalImg.classList.add("modal-image") 
+
+        var span = document.getElementsByClassName("close")[0];
+        span.onclick = function() { 
+        modal.style.display = "none";
+        }
+    }
+
     const onSuccess = (res) => {
         var gif = document.querySelector('.search-result')
         for (i = 0; i <= 10; i++) {
             var displayGif = gif.appendChild(document.createElement("img"))
             displayGif.src = res.data[i].images.fixed_height.url
             displayGif.classList.add("giphy")  
-        }
-
-        var modal = document.getElementById('myModal');
-        function getModal() {
-            modal.style.display = "block";
-            var modalCaption = modal.appendChild(document.createElement("p"))
-            modalCaption.textContent = event.target.src
-            modalCaption.id = "caption"
-
-            var modalImg = modal.appendChild(document.createElement("img"))
-            modalImg.src = event.target.src
-            modalImg.classList.add("modal-image") 
-
-            var span = document.getElementsByClassName("close")[0];
-            span.onclick = function() { 
-            $("#caption").remove()
-            $(".modal-image").remove()
-            modal.style.display = "none";
-            }
         }
 
         $(".giphy").on('click', function(event) {
@@ -48,6 +50,7 @@ $(document).ready(function() {
         getResults()
         searchResultInstructions = document.querySelector("p")
         searchResultInstructions.classList.remove('hidden')
+        hideResetBtn.classList.remove('hidden')
     })
 
     $(window).scroll(function() {
@@ -56,6 +59,11 @@ $(document).ready(function() {
             getResults()
         }
     })
+
+    $("#reset").click(function(){
+        document.location.reload(true);
+        hideResetBtn.classList.add('hidden')
+    });
 })
     
 
