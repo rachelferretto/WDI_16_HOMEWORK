00577c0161e72a3e7b86d90b2b6a3c404aca36f1
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-08-07 22:08:57 +1000

    delete and edit functions

--- Rachel/wk08/2-tue/planets_II/app/controllers/planets_controller.rb
+++ Rachel/wk08/2-tue/planets_II/app/controllers/planets_controller.rb
@@ -24,13 +24,28 @@ class PlanetsController < ApplicationController
 
 
     def destroy
+        @planet = Planet.find (params[:id])
+        if @planet.destroy
+            redirect_to '/planets'
+        else 
+            render :show
+        end
     end
 
     def edit
         @planet = Planet.find( params[:id])
+        render :edit
     end
 
     def update
+        @planet = Planet.find( params[:id])
+        @planet.name = params[:name]
+        @planet.url = params[:url]
+        if @planet.save
+            redirect_to "/planets/#{@planet.id}"
+        else
+            render :edit
+        end
     end
 
 
